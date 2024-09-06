7b9259f7c71315066cf100e6705ad190dce0eb78
felipeggoncales <felipeggoncales22@gmail.com>
2024-09-06 17:29:06 -0300

    correcao-exibir-imagens
    corrigindo a exibição de imagens ao pesquisar os animais

--- app.js
+++ app.js
@@ -7,23 +7,24 @@ function click() {
     let mensagem = ``;
 
     for (let animal of dados) {
-        if (!searchInput) {
-            secSearch.style.display = 'none';
-            secInfo.style.display = 'flex';
-            mensagem = `<p class='text-na'>Nenhum animal encontrado</p>`;
-        } else if (animal.nome.toLowerCase().includes(searchInput)) {
+        if (animal.nome.toLowerCase().includes(searchInput)) {
             secSearch.style.display = 'none';
             secInfo.style.display = 'flex';
             mensagem += `
             <div class='border'>
                 <h2>${animal.nome}</h2>
-                <p><span>Nome Científico:</span> <i>${animal.nomeCientifico}</i></p>
-                <p><span>Habitat:</span> ${animal.habitat}</p>
-                <p><span>Origem:</span> ${animal.origem}</p>
-                <p><span>Descrição:</span> ${animal.descricao}</p>
+                <p><span>Nome Científico:</span><br><i>${animal.nomeCientifico}</i></p>
+                <p><span>Habitat:</span><br>${animal.habitat}</p>
+                <p><span>Origem:</span><br>${animal.origem}</p>
+                <p><span>Descrição:</span><br>${animal.descricao}</p>
             </div>
             `;
-            body.style.backgroundImage = animal.img;
+        }
+    }
+
+    for (let animalInvertidos of [...dados].reverse()) {
+        if (animalInvertidos.nome.toLowerCase().includes(searchInput)) {
+            body.style.backgroundImage = `url(${animalInvertidos.img})`;
         }
     }
 
@@ -31,12 +32,19 @@ function click() {
         secSearch.style.display = 'none';
         secInfo.style.display = 'flex';
         mensagem = `<p class='text-na'>Nenhum animal encontrado</p>`;
-        body.style.backgroundImage = 'url(../img/bg-coala.png)';
+        body.style.backgroundImage = 'url(assets/img/bg-padrao.jpg)';
     }
 
     secInfo.innerHTML = mensagem;
 }
 
+function preloadImage(url) {
+    const img = new Image();
+    img.src = url;
+    console.log(img)
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('search-icon').addEventListener('click', click);
+    document.getElementById('search-input').addEventListener('keydown', click)
+    dados.forEach(animal => preloadImage(animal.img));
 });
--- banco.js
+++ banco.js
@@ -9,7 +9,7 @@ let dados = [
         de krill, sendo conhecida por suas vocalizações profundas. Atualmente, a rainha dos mares enfrenta
         ameaças devido à caça comercial humana com o intuito de extrair o chamado "Óleo de Baleia", muito
         utilizado na indústria para iluminação, lubrificação de máquinas e produção de sabão.`,
-        img: 'url(../img/baleia-azul.png)'
+        img: 'assets/img/baleia-azul.png'
     },
     {
         nome: `Tigre-de-Bengala`,
@@ -19,7 +19,8 @@ let dados = [
         descricao: `O tigre-de-Bengala é uma das maiores subespécies de tigres, podendo atingir até 3 metros de
         comprimento e pesar até 250 kg. É conhecido por sua pelagem laranja com listras pretas e vive em florestas
         e áreas de vegetação densa. Enfrenta perigo devido à caça furtiva e perda de habitat, sendo caçado por sua
-        pele e outros produtos.`
+        pele e outros produtos.`,
+        img: 'assets/img/tigre-bengala.png'
     },
     {
         nome: `Rinoceronte-de-Java`,
@@ -28,16 +29,18 @@ let dados = [
         origem: `Ásia`,
         descricao: `O rinoceronte-de-Java é um dos rinocerontes mais ameaçados, com menos de 75 indivíduos restantes na
         natureza. Possui uma pele cinza escura e é o menor dos rinocerontes vivos. Habita florestas tropicais e
-        enfrenta ameaças de perda de habitat e caça ilegal por seus chifres, usados em medicina tradicional.`
+        enfrenta ameaças de perda de habitat e caça ilegal por seus chifres, usados em medicina tradicional.`,
+        img: 'assets/img/rino-de-java.png'
     },
     {
         nome: `Panda Gigante`,
         nomeCientifico: `Ailuropoda melanoleuca`,
         habitat: `Terrestre`,
         origem: `China`,
-        descricao: `O panda gigante é conhecido por sua pelagem preta e branca e é um símbolo de conservação. Pode
+        descricao: `O panda gigante é conhecido por sua pelagem preta e branca e é um sím   bolo de conservação. Pode
         pesar até 160 kg e se alimenta principalmente de bambu. Enfrenta ameaças devido à perda de habitat e baixa
-        taxa de reprodução na natureza. A proteção de suas áreas de floresta é crucial para sua sobrevivência.`
+        taxa de reprodução na natureza. A proteção de suas áreas de floresta é crucial para sua sobrevivência.`,
+        img: 'assets/img/panda-gigante.png'
     },
     {
         nome: `Coala`,
@@ -48,7 +51,7 @@ let dados = [
         arbóreos. Alimenta-se exclusivamente de folhas de eucalipto e vive em áreas de floresta. Enfrenta ameaças de
         perda de habitat devido a incêndios florestais e desenvolvimento urbano, além de doenças que afetam suas
         populações.`,
-        img: 'url(../img/bg-coala.png)'
+        img: 'assets/img/coala.png'
     },
     {
         nome: `Corvo-de-cabeça-branca`,
@@ -57,7 +60,8 @@ let dados = [
         origem: `Oceania`,
         descricao: `O corvo-de-cabeça-branca é uma ave endêmica da Ilha de Norfolk, conhecida por sua plumagem
         distinta e tamanho robusto. Vive em áreas florestais e enfrenta ameaças devido à perda de habitat e predadores
-        invasivos. Seus números estão em declínio e necessita de proteção para sua sobrevivência.`
+        invasivos. Seus números estão em declínio e necessita de proteção para sua sobrevivência.`,
+        img: 'assets/img/corvo-cabeca-branca.png'
     },
     {
         nome: `Elefante-de-Sumatra`,
@@ -66,7 +70,8 @@ let dados = [
         origem: `Sumatra`,
         descricao: `O elefante-de-Sumatra é uma das subespécies de elefantes asiáticos e é conhecido por seu tamanho
         menor em comparação com outros elefantes asiáticos. Habita florestas tropicais e enfrenta ameaças de
-        desmatamento e caça. A conservação de seus habitats naturais é essencial para sua sobrevivência.`
+        desmatamento e caça. A conservação de seus habitats naturais é essencial para sua sobrevivência.`,
+        img: 'assets/img/elefante-sumatra.png'
     },
     {
         nome: `Tartaruga-de-pente`,
@@ -75,7 +80,8 @@ let dados = [
         origem: `Tropicais e subtropicais`,
         descricao: `A tartaruga-de-pente é uma das tartarugas marinhas mais ameaçadas, com uma carapaça distintiva
         e colorida. Vive em recifes de corais e áreas costeiras tropicais. É caçada por sua concha, usada para
-        ornamentação e joias, além de enfrentar ameaças como a poluição e a destruição dos recifes de corais.`
+        ornamentação e joias, além de enfrentar ameaças como a poluição e a destruição dos recifes de corais.`,
+        img: 'assets/img/tartaruga-de-pente.png'
     },
     {
         nome: `Macaco-narigudo`,
@@ -84,7 +90,8 @@ let dados = [
         origem: `Borneu`,
         descricao: `O macaco-narigudo é conhecido por seu nariz grande e distinto e vive em áreas de mangue e florestas
         costeiras. Enfrenta ameaças devido à perda de habitat e caça. A proteção das florestas de mangue é crucial
-        para a conservação desta espécie única.`
+        para a conservação desta espécie única.`,
+        img: 'assets/img/macaco-narigudo.png'
     },
     {
         nome: `Peixe-lua`,
@@ -93,7 +100,8 @@ let dados = [
         origem: `Oceano Atlântico e Pacífico`,
         descricao: `O peixe-lua é o maior peixe ósseo do mundo, com um corpo achatado e formato distinto. Pode
         pesar até 2.300 kg e é encontrado em águas oceânicas abertas. Enfrenta ameaças como captura acidental em
-        redes de pesca e poluição dos oceanos. A proteção das áreas oceânicas é importante para sua conservação.`
+        redes de pesca e poluição dos oceanos. A proteção das áreas oceânicas é importante para sua conservação.`,
+        img: 'assets/img/peixe-lua.png'
     },
     {
         nome: `Jaguar`,
@@ -102,7 +110,8 @@ let dados = [
         origem: `América Central e do Sul`,
         descricao: `O jaguar é o maior felino das Américas, com uma pelagem manchada e potente capacidade de caça. 
         Vive em florestas tropicais e áreas de cerrado. Enfrenta ameaças de desmatamento e caça ilegal, com
-        seus números diminuindo rapidamente. A preservação de seu habitat natural é crucial para sua sobrevivência.`
+        seus números diminuindo rapidamente. A preservação de seu habitat natural é crucial para sua sobrevivência.`,
+        img: 'assets/img/jaguar.png'
     },
     {
         nome: `Leão`,
@@ -111,7 +120,8 @@ let dados = [
         origem: `África`,
         descricao: `O leão é um dos grandes felinos mais icônicos, conhecido por sua juba e comportamento social em
         grupos chamados de "alcateias". Vive principalmente em savanas e áreas abertas. Enfrenta ameaças de perda
-        de habitat e caça, com seus números diminuindo devido a conflitos com humanos e perda de presas naturais.`
+        de habitat e caça, com seus números diminuindo devido a conflitos com humanos e perda de presas naturais.`,
+        img: 'assets/img/leao.png'
     },
     {
         nome: `Elefante-Africano`,
@@ -121,7 +131,8 @@ let dados = [
         descricao: `O elefante-africano é o maior mamífero terrestre, conhecido por suas grandes orelhas e presas de
         marfim. Habita savanas e florestas africanas. Enfrenta ameaças de caça furtiva por marfim e perda de habitat
         devido à expansão agrícola e urbana. A conservação de seus habitats e a luta contra a caça furtiva são
-        essenciais para sua sobrevivência.`
+        essenciais para sua sobrevivência.`,
+        img: 'assets/img/elefante-africano.png'
     },
     {
         nome: `Gorila`,
@@ -130,7 +141,8 @@ let dados = [
         origem: `África Central`,
         descricao: `O gorila é o maior dos primatas vivos, conhecido por sua força e comportamento social complexo. 
         Vive em florestas tropicais e enfrenta ameaças como perda de habitat, doenças e caça ilegal. A proteção das
-        florestas e esforços para combater doenças são cruciais para sua conservação.`
+        florestas e esforços para combater doenças são cruciais para sua conservação.`,
+        img: 'assets/img/gorila.png'
     },
     {
         nome: `Pinguim-imperador`,
@@ -140,7 +152,8 @@ let dados = [
         descricao: `O pinguim-imperador é o maior dos pinguins e é conhecido por suas impressionantes habilidades de
         mergulho e sua adaptação ao frio extremo da Antártica. Enfrenta ameaças devido às mudanças climáticas que afetam
         o gelo marinho e a disponibilidade de alimentos. A conservação do ambiente antártico é crucial para sua
-        sobrevivência.`
+        sobrevivência.`,
+        img: 'assets/img/pinguin-imperador.png'
     },
     {
         nome: `Urso Polar`,
@@ -149,6 +162,7 @@ let dados = [
         origem: `Ártico`,
         descricao: `O urso polar é um predador apex do Ártico, adaptado para caçar focas e sobreviver em temperaturas
         extremamente baixas. Enfrenta ameaças de perda de habitat devido ao derretimento do gelo marinho causado pelas
-        mudanças climáticas. A proteção do habitat ártico é essencial para a conservação desta espécie.`
+        mudanças climáticas. A proteção do habitat ártico é essencial para a conservação desta espécie.`,
+        img: 'assets/img/urso-polar.png'
     }
 ];
